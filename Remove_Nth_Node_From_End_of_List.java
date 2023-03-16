9c3cf3fbcb34a2b7844752539c73c17047f1ad36
Keshav <110564015+keshavsolankii@users.noreply.github.com>
2023-03-17 00:50:18 +0530

    Update Remove_Nth_Node_From_End_of_List.java

--- Remove_Nth_Node_From_End_of_List.java
+++ Remove_Nth_Node_From_End_of_List.java
@@ -1,3 +1,5 @@
+// Given the head of a linked list, remove the nth node from the end of the list and return its head.
+
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
         ListNode start = new ListNode();
@@ -6,16 +8,16 @@ class Solution {
         ListNode fast = start;
         ListNode slow = start;
         
-        for(int i=0; i<n; i++)
+        for(int i=0; i<n; i++) // run the fast pointer for n times
         {
             fast = fast.next;
         }
         while(fast.next != null)
         {
             fast = fast.next;
-            slow = slow.next;
+            slow = slow.next; // slow pointer stops at one node before the node to be removed
         }
-        slow.next = slow.next.next;
+        slow.next = slow.next.next; // connect the next of the node to the next of next node
         
         return start.next;
     }
